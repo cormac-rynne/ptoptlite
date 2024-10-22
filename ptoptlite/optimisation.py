f0e95e65ab0fb83f94da269ea5d015f5766b2be0
Cormac Rynne <crynne@expediagroup.com>
2024-10-22 12:18:43 +0100

    feat: customiseable objective function

--- ptoptlite/optimisation.py
+++ ptoptlite/optimisation.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Callable
 
 import torch
 import torch.nn as nn
@@ -11,6 +11,10 @@ def optimise_input(
     model: nn.Module,
     lower_bound: torch.Tensor,
     upper_bound: torch.Tensor,
+    objective_fn: Callable[
+        [torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+        torch.Tensor,
+    ],
     n_opt_epochs: int = 1000,
     learning_rate: float = 0.1,
     device: torch.device = torch.device("cpu"),
@@ -24,6 +28,7 @@ def optimise_input(
         model (nn.Module): The trained model.
         lower_bound (torch.Tensor): Lower bound for x.
         upper_bound (torch.Tensor): Upper bound for x.
+        objective_fn (Callable): A function that computes the loss given x_opt, y_opt, x, y, and device.
         n_opt_epochs (int, optional): Number of optimisation epochs. Defaults to 1000.
         learning_rate (float, optional): Learning rate for the optimiser. Defaults to 0.1.
         device (torch.device, optional): Device to perform optimisation on. Defaults to CPU.
@@ -31,6 +36,8 @@ def optimise_input(
     Returns:
         Tuple[torch.Tensor, torch.Tensor]: Optimised x and corresponding y.
     """
+
+    lower_bound, upper_bound = lower_bound.to(device), upper_bound.to(device)
     model.to(device)
     x_opt = x.clone().detach().requires_grad_(True).to(device)
     optimiser_opt = optim.Adam([x_opt], lr=learning_rate)
@@ -38,16 +45,15 @@ def optimise_input(
     for epoch in range(1, n_opt_epochs + 1):
         optimiser_opt.zero_grad()
         y_opt = model(x_opt)
-        loss = -y_opt.sum() + torch.abs(x_opt.sum() - x.sum().to(device))
+        loss = objective_fn(x_opt, y_opt, x, y)
         loss.backward()
         optimiser_opt.step()
 
         with torch.no_grad():
-            x_opt.clamp_(lower_bound.to(device), upper_bound.to(device))
+            x_opt.clamp_(lower_bound, upper_bound)
 
         if epoch % 100 == 0 or epoch == 1:
-            total_y = y.sum().item()
-            print(f"Optimisation Epoch {epoch}/{n_opt_epochs}, Total y: {total_y:.2f}")
+            print(f"Optimisation Epoch {epoch}/{n_opt_epochs}, Loss: {loss.item():.2f}")
 
     optimised_x = x_opt.detach()
     optimised_y = model(optimised_x).detach()
